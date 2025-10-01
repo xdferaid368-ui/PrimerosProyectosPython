350a1dce6d3439ddd50933bf14b70e3b30efe125
xdferaid368-ui <xdferaid368@ieshnosmachado.org>
2025-10-01 14:41:37 +0200

    Clase 1 de nov

--- Ejdiccionarios.py
+++ Ejdiccionarios.py
@@ -1,27 +1,54 @@
-videojuegos = []
-nvideo = int(input("Numero de videojuegos a añadir: "))
+# videojuegos = []
+# nvideo = int(input("Numero de videojuegos a añadir: "))
 
-for i in range(nvideo):
-    nombre = input("Introduzca el nombre: ")
-    valoracion = int(input("Valoracion: "))
-    categoria = input("Categoria: ")
+# for i in range(nvideo):
+#     nombre = input("Introduzca el nombre: ")
+#     valoracion = int(input("Valoracion: "))
+#     categoria = input("Categoria: ")
 
-    videojuego = {"nombre": nombre, "valoracion": valoracion, "categoria": categoria}
-    videojuegos.append(videojuego)
+#     videojuego = {"nombre": nombre, "valoracion": valoracion, "categoria": categoria}
+#     videojuegos.append(videojuego)
 
-# Crear listas en una sola línea
-nombres = [v["nombre"] for v in videojuegos]
-valoraciones = [v["valoracion"] for v in videojuegos]
+# # Crear listas en una sola línea
+# nombres = [i["nombre"] for i in videojuegos]
+# valoraciones = [i["valoracion"] for i in videojuegos]
 
-print("Lista de videojuegos (diccionarios):")
-print(videojuegos)
+# print("Lista de videojuegos:", (videojuegos))
+# print("Lista de nombres:", (nombres))
+# print("Lista de valoraciones:",(valoraciones))
+# media = sum(valoraciones) / len(valoraciones)
+# print("La media de las valoraciones es:", media)
 
-print("Lista de nombres:")
-print(nombres)
+asignatura1={"nombre":"servidor","profesor":"JI","horas":7}
+asignatura2={"nombre":"cliente","profesor":"David","horas":5}
 
-print("Lista de valoraciones:")
-print(valoraciones)
+persona={"nombre":"David","edad":20,"esprofe":False,"asignaturas":["cliente","servidor"],"Notas":{"cliente":4.4,"servidor":2}}
+persona2={"nombre":"Migue","edad":20,"esprofe":False,"asignaturas":["cliente","servidor"],"Notas":{"cliente":5,"servidor":3.4}}
 
-# Media usando directamente la lista de valoraciones
-media = sum(valoraciones) / len(valoraciones)
-print("La media de las valoraciones es:", media)
+alumno=[]
+alumno.append(persona)
+alumno.append(persona2)
+
+for a in persona:
+    suma=0
+    cantidad=0
+    for nota in a["Notas"].values():
+        suma=suma+nota
+        cantidad=cantidad+1
+    media=suma/cantidad
+    print("Alumno:",a["nombre"])
+    print("Media:",media)
+    print("Suspensos:")
+    for asignatura,nota in a["Notas"].items():
+        if nota<5:
+            print(asignatura,nota)
+# pablo = persona
+# pablo["nombre"]="Pablo"
+# print(persona["nombre"])
+# print(pablo["nombre"])
+
+
+# print(alumno[0]["asignaturas"][0]["horas"])
+
+# asignatura1.pop("horas")
+# print(asignatura1) 
